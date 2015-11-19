f5f23e3143020e2ea1e865761a2953384c41f653
khorne55 <root@localhost.localdomain>
2015-11-19 17:50:16 -0500

    wordAnalyzer now distinguishes unique words

--- wordAnalyzer.c
+++ wordAnalyzer.c
@@ -14,18 +14,31 @@ The aim of this module is to perform various operations on a text file.
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #define WORD "A-Za-z"
 
 int countWords(FILE *f){
    char ch[100][100];
+   int cmp,x;
    int count = 0;
        while (fscanf(f, "%[^" WORD "]",ch[count]) != EOF
         && fscanf(f, "%10[" WORD "]",ch[count]) != EOF) {
+
+        if(count>0) {
+              printf("%d\n",count );
+
+        for(x=0;x<count;x++) {
+
+          if((strcasecmp(ch[count],ch[x]))==0)
+
+              printf("String is equal\n");
+        }
+      }
+        puts(ch[count]);
         count++;
-        puts(ch[count-1]);
     }
-    puts(ch[7]);
+    puts(ch[13]);
 
    return count;
 }
