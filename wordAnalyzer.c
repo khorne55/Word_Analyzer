97b6c06303a119fc500616fbab836aee9ecedc50
khorne55 <root@localhost.localdomain>
2015-11-19 19:51:47 -0500

    Now displays proper amount of unique words

--- wordAnalyzer.c
+++ wordAnalyzer.c
@@ -18,37 +18,53 @@ The aim of this module is to perform various operations on a text file.
 
 #define WORD "A-Za-z"
 
-int countWords(FILE *f){
+void hello(int*a,int* b) {
+*a=2;
+*b=3;
+(*a)++;
+a++;
+}
+
+int countWords(FILE *f,int* now){
    char ch[100][100];
-   int cmp,x;
+   int x;
+   *now=0;
    int count = 0;
        while (fscanf(f, "%[^" WORD "]",ch[count]) != EOF
         && fscanf(f, "%10[" WORD "]",ch[count]) != EOF) {
 
         if(count>0) {
-              printf("%d\n",count );
 
         for(x=0;x<count;x++) {
 
-          if((strcasecmp(ch[count],ch[x]))==0)
-
+          if((strcasecmp(ch[count],ch[x]))==0) {
+            if(fscanf(f, "%[^" WORD "]",ch[count]) != EOF
+            && fscanf(f, "%10[" WORD "]",ch[count]) != EOF) 
               printf("String is equal\n");
+              (*now)++;
+          }
         }
       }
         puts(ch[count]);
         count++;
+        (*now)++;
     }
     puts(ch[13]);
+    printf("%d\n",*now );
 
    return count;
 }
 
 int main(void){
 
-   int wordCount = 0;
+   int uniquewordCount=0,wordCount=0;
    FILE *rFile = fopen("test2.txt", "r");
-   wordCount += countWords(rFile);
-   printf("%d\n", wordCount);
+   uniquewordCount += countWords(rFile,&wordCount);
+   int a,b;
+   hello(&a,&b);
+   printf("%d\n",(a+b));
+   printf("Amount of unique words: %d\n", uniquewordCount);
+   printf("Amount of words: %d\n", wordCount);
    return 0;
 }
 
